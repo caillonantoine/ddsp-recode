9d7c92d39429a1fd49a3b244d674037b60618b8c
Antoine <caillonantoine@gmail.com>
2019-10-07 11:05:01 +0200

    Added differentiable multiscaleFFT, and synth

--- ddsp.py
+++ ddsp.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import numpy as np
-
+from hparams import preprocess
 
 class MLP(nn.Module):
     def __init__(self, in_size=512, out_size=512, loop=3):
@@ -42,14 +42,100 @@ class Decoder(nn.Module):
 
         x = self.fi_MLP(x)
 
-        return self.dense(x)
+        alpha = torch.sigmoid(self.dense(x))
+        print(alpha.shape)
+
+        return alpha / torch.sum(alpha,-1).unsqueeze(-1)
 
 class NeuralSynth(nn.Module):
-    def __init__(self, n_partial, fmin, fmax):
+    def __init__(self, n_partial):
+        super().__init__()
         self.decoder = Decoder(512, n_partial)
+        self.condition_upsample = nn.Upsample(scale_factor=preprocess.block_size,
+                                              mode="linear")
+        self.k = torch.arange(1, n_partial + 1)\
+                      .reshape(1,1,-1)\
+                      .float()\
+                      .to(self.decoder.dense.weight.device)
+
+        self.windows = [
+            torch.from_numpy(
+                np.hanning(scale)
+            ).float().to(self.decoder.dense.weight.device)\
+            for scale in preprocess.fft_scales
+        ]
+
+
+    def forward(self, f0, lo):
+        assert len(f0.shape)==3, f"f0 input must be 3-dimensional, but is {len(f0.shape)}-dimensional."
+        assert len(lo.shape)==3, f"lo input must be 3-dimensional, but is {len(lo.shape)}-dimensional."
+
+        alpha = self.decoder(f0, lo)
+
+        f0 = self.condition_upsample(f0.transpose(1,2)).squeeze(1)/preprocess.samplerate
+        lo = self.condition_upsample(lo.transpose(1,2)).squeeze(1)
+        alpha = self.condition_upsample(alpha.transpose(1,2)).transpose(1,2)
+
+        phi = torch.zeros(f0.shape).to(f0.device)
+
+        for i in np.arange(1,phi.shape[-1]):
+            phi[:,i] = 2 * np.pi * f0[:,i] + phi[:,i-1]
+
+        phi = phi.unsqueeze(-1).expand(alpha.shape)
+
+        y =  lo * torch.sum(alpha * torch.sin( self.k * phi),-1)
+
+        return y
+
+    def multiScaleFFT(self, x, overlap=75/100):
+        stfts = []
+        amp = lambda x: torch.sqrt(x[:,:,:,0]**2 + x[:,:,:,1]**2)
+        for i,scale in enumerate(preprocess.fft_scales):
+            stfts.append(amp(
+                torch.stft(x,
+                           n_fft=scale,
+                           window=self.windows[i],
+                           hop_length=int((1-overlap)*scale),
+                           center=False)
+            ))
+        return stfts
+
+
 
 if __name__ == '__main__':
-    dec = Decoder(512, 100)
-    f0  = torch.randn(1,250,1)
-    lo  = torch.randn(1,250,1)
-    dec(f0,lo)
+    import matplotlib.pyplot as plt
+    import librosa as li
+    import sounddevice as sd
+    plt.ion()
+
+    ns = NeuralSynth(50)
+    f0 = torch.from_numpy(np.linspace(110,220,preprocess.sequence_size).reshape(1,-1,1)).float()
+    lo = torch.from_numpy(np.linspace(1,0,preprocess.sequence_size).reshape(1,-1,1)).float()
+    output = ns(f0,lo)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
