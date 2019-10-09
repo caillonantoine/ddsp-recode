bf48d0b3ab6124f9dd2aa5d9a735ff894947b83a
Antoine <caillonantoine@gmail.com>
2019-10-09 15:51:21 +0200

    Updated hparams

--- hparams.py
+++ hparams.py
@@ -1,16 +1,26 @@
 class preprocess:
-    input_filename = "data/cello.wav" # must be a mono, 44100Hz .wav file
+    input_filename = "data/cello.wav" # must be a mono, 16000Hz .wav file
     samplerate     = 16000 # Used when synth back audio
     output_dir     = "output"
     crepe_f0       = f"{output_dir}/cello.f0.csv"
 
-    fft_scales     = [2048, 1024, 512, 256, 128, 64] # Multi scale stft objective
-    block_size     = 64 # Must be the same block size than that of crepe !!
-    sequence_size  = 400 # Number of sequence to process in the GRU cell
-    num_batch      = 1125 # Must be changed after preprocessing......
+    # Multi scale stft objective
+    fft_scales     = [2048, 1024, 512, 256, 128, 64]
+
+    # Must be the same block size than that of crepe !!
+    block_size     = 64
+
+    # Number of sequence to process in the GRU cell
+    sequence_size  = 400
+
+    # Must match the number displayed when preprocessing:
+    # "Splitting data into XXXXX examples..."
+    num_batch      = 1125
 
 class ddsp:
+    # Number of partials involved in the harmonic signal
     n_partial      = 100
+
     hidden_size    = 512
     filter_size    = 64
     impulse_time   = 2 # In second
