b40a1ddab961d9ea7c2f2600ad1b0d9fd3a09db8
Antoine <caillonantoine@gmail.com>
2019-10-14 15:06:31 +0200

    Working patch, no noise

--- realtime_patch/ddsp~.cpp
+++ realtime_patch/ddsp~.cpp
@@ -1,32 +1,83 @@
 #include "m_pd.h"
 #include "ddsp_core.hpp"
+#include <math.h>
 #include <iostream>
 
 #define PD_BLOCK_SIZE 256
 #define MODEL_BLOCK_SIZE 160
+
 #define SAMPLING_RATE 16000
+#define PI 3.14159265359
 
 static t_class *ddsp_tilde_class;
+
+void sayhello()
+{
+  std::cout << "hello" << std::endl;
+}
 
 
 typedef struct _ddsp_tilde {
   t_object x_obj;
   t_sample f;
 
+
   float *buffer;
-  float phase;
+  float phase, f0, lo;
 
   float result[1 + PARTIAL_NUMBER + FILTER_SIZE];
 
   int bufferReadHead;
   int bufferWriteHead;
-  int nextCondition;
+  int currentCondition;
 
   DDSPCore *ddsp;
 
   t_inlet  *x_in2;
   t_outlet *x_out;
 } t_ddsp_tilde;
+
+void processWorker(t_ddsp_tilde *x, t_sample *in1, t_sample *in2){
+
+  float f0, lo, t;
+
+  while (x->currentCondition < PD_BLOCK_SIZE) {
+    f0 = in1[x->currentCondition];
+    lo = in2[x->currentCondition];
+
+    x->ddsp->getNextOutput(f0, lo, x->result);
+
+    for (int i(0); i<MODEL_BLOCK_SIZE; i++) {
+      t = i / float(MODEL_BLOCK_SIZE);
+
+      x->phase += 2 * PI * (x->f0 * (1-t) + f0 * t) / float(SAMPLING_RATE);
+      while (x->phase >= 2*PI) x->phase -= 2*PI;
+
+      x->buffer[x->bufferWriteHead] = 0;
+      for (int p(1); p<PARTIAL_NUMBER; p++){
+        x->buffer[x->bufferWriteHead] += \
+        x->result[0] * x->result[p] * sin(p * x->phase);
+
+        if (p*f0/SAMPLING_RATE > 0.5) {
+          break;
+        }
+      }
+
+      x->bufferWriteHead = (x->bufferWriteHead + 1) % (3 * PD_BLOCK_SIZE);
+    }
+
+    x->bufferWriteHead = x->bufferWriteHead % (3 * PD_BLOCK_SIZE);
+
+    x->f0 = f0;
+    x->lo = lo;
+
+    x->currentCondition += MODEL_BLOCK_SIZE;
+
+  }
+}
+
+
+
 
 t_int *ddsp_tilde_perform(t_int *w)
 {
@@ -36,29 +87,13 @@ t_int *ddsp_tilde_perform(t_int *w)
   t_sample  *out =    (t_sample *)(w[4]);
   int          n =           (int)(w[5]);
 
+  processWorker(x, in1, in2);
+
   while (n--) *out++ = x->buffer[x->bufferReadHead++];
 
-  // GET NEW SAMPLES AND WRITE THEM TO (POTENTIALLY OVERLAPPING) CURRENT WRITE
-  // BUFFER.
 
-  float f0[2];
-  float lo[2];
-
-  while (x->nextCondition < PD_BLOCK_SIZE) {
-    f0[0] = f0[1];
-    lo[0] = lo[1];
-    f0[1] = in1[x->nextCondition];
-    lo[1] = in2[x->nextCondition];
-
-    x->ddsp->getNextOutput(in1[x->nextCondition], in2[x->nextCondition], x->result);
-    x->nextCondition += MODEL_BLOCK_SIZE;
-
-    
-  }
-
-  x->nextCondition = x->nextCondition % PD_BLOCK_SIZE;
-  x->bufferReadHead = x->bufferReadHead % (2 * PD_BLOCK_SIZE);
-  x->bufferWriteHead = PD_BLOCK_SIZE - x->bufferReadHead;
+  x->currentCondition = x->currentCondition % PD_BLOCK_SIZE;
+  x->bufferReadHead = x->bufferReadHead % (3 * PD_BLOCK_SIZE);
 
 
   return (w+6);
@@ -82,12 +117,16 @@ void *ddsp_tilde_new(t_floatarg f)
 {
   t_ddsp_tilde *x = (t_ddsp_tilde *)pd_new(ddsp_tilde_class);
 
-  x->buffer = (float *) malloc(2 * PD_BLOCK_SIZE * sizeof(float));
-  x->phase  = 0;
+  x->buffer = (float *) malloc(3 * PD_BLOCK_SIZE * sizeof(float));
+  for (int i(0); i<3*PD_BLOCK_SIZE; i++) x->buffer[i] = 0;
+
+  x->phase = 0;
+  x->f0    = 0;
+  x->lo    = 0;
 
   x->bufferReadHead  = 0;
   x->bufferWriteHead = PD_BLOCK_SIZE;
-  x->nextCondition   = 0;
+  x->currentCondition   = 0;
 
   x->ddsp = new DDSPCore();
 
