ad665f9fddd4e6cca04d78e980f0801299ff5ab8
Antoine <caillon@ircam.fr>
2019-10-30 15:17:34 +0100

    Changed preprocess routine (switch to DIO)

--- preprocess.py
+++ preprocess.py
@@ -6,17 +6,59 @@ import os
 from torch_ddsp.hparams import preprocess
 from torch_ddsp.ddsp import NeuralSynth
 import torch
+from glob import glob
+from pyworld import dio
 
 multiScaleFFT = NeuralSynth().multiScaleFFT
 amp = lambda x: x[:,:,0]**2 + x[:,:,1]**2
 
+def getSmoothLoudness(x, block_size, kernel_size=8):
+    win = np.hamming(kernel_size)
+
+    x = x.reshape(-1, block_size)
+    lo = np.log(np.mean(x**2, -1) + 1e-15)
+    lo = lo.reshape(-1)
+
+    lo = np.convolve(lo, win, "same")
+    return lo
+
+def getFundamentalFrequency(x):
+    sr         = preprocess.samplerate
+    block_size = preprocess.block_size
+    hop = int(1000 * block_size / sr)
+
+    f0 = dio(x.astype(np.float64), sr,
+             frame_period=hop,
+             f0_floor=50,
+             f0_ceil=2000)[0]
+
+    return f0[:preprocess.sequence_size].astype(np.float)
+
+class BatchSoundFiles:
+    def __init__(self, wav_list):
+        self.wavs = [sf.SoundFile(file) for file in wav_list]
+        self.file_head = 0
+
+    def read(self, N):
+        x = self.wavs[self.file_head].read(N)
+        if len(x) != N:
+            self.file_head += 1
+            x = self.wavs[self.file_head].read(N)
+
+        return self.file_head, x
+
+    def __len__(self):
+        mod = preprocess.block_size * preprocess.sequence_size
+        return sum([mod*(len(wav)//mod) for wav in self.wavs])
+
+
 def process(filename, block_size, sequence_size):
     os.makedirs("output", exist_ok=True)
 
-    sound = sf.SoundFile(filename)
+    sound = BatchSoundFiles(glob(filename))
     batch = len(sound) // (block_size * sequence_size)
-
     print(f"Splitting data into {batch} examples of {sequence_size}-deep sequences of {block_size} samples.")
+
 
     scales = preprocess.fft_scales
     output = preprocess.output_dir
@@ -27,23 +69,44 @@ def process(filename, block_size, sequence_size):
                             shape=(batch, ex_sp.shape[0], ex_sp.shape[1]),
                             mode="w+"))
 
-    lo = np.zeros([batch,sequence_size])
+    lo        = np.zeros([batch,sequence_size])
+    f0        = np.zeros([batch, sequence_size])
+    raw_audio = np.zeros([batch, sequence_size*block_size])
+    index     = np.zeros([batch])
+
+    in_point  = 0
+    last_file = 0
 
     for b in tqdm(range(batch)):
-        x = sound.read(block_size * sequence_size)
+        file_index, x = sound.read(block_size * sequence_size)
+        index[b] = file_index
+
         for i,msstft in enumerate(multiScaleFFT(torch.from_numpy(x).float(), amp=amp)):
             sp[i][b,:,:] = msstft.detach().numpy()
 
-        x = x.reshape(-1, block_size)
-        for i,seq in enumerate(x):
-            lo[b,i] = np.log(np.mean(seq**2)+1e-15)
+        lo[b,:] = getSmoothLoudness(x, preprocess.block_size, preprocess.kernel_size)
+        f0[b,:] = getFundamentalFrequency(x)
 
-    mean_loudness = np.mean(lo)
-    std_loudness  = np.std(lo)
-    lo -= mean_loudness
-    lo /= std_loudness
+        raw_audio[b,:] = x
+
+        if (file_index != last_file) or (b==batch-1):
+            # print(f"\nnormalization", in_point, b)
+            last_file = file_index
+
+            mean_loudness = np.mean(lo[in_point:b])
+            std_loudness  = np.std(lo[in_point:b])
+
+            lo[in_point:b] -= mean_loudness
+            lo[in_point:b] /= std_loudness
+
+            in_point = b
+
+
 
     np.save(f"{output}/lo.npy", lo)
+    np.save(f"{output}/f0.npy", f0)
+    np.save(f"{output}/index.npy", index)
+    np.save(f"{output}/raw_audio.npy", raw_audio)
 
 
 if __name__ == '__main__':
