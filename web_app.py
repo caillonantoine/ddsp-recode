677890ffc8e5f53262679a57dded86123151dacf
Antoine <caillon@ircam.fr>
2020-12-08 18:24:24 +0100

    remove eport from git

--- web_app.py
+++ web_app.py
@@ -12,9 +12,9 @@ UPLOAD_FOLDER = "./tmp/"
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-model = torch.jit.load("ddsp_smooth_loudness_A_weight_pretrained.ts")
-sampling_rate = model.sampling_rate.item()
-block_size = model.block_size.item()
+model = torch.jit.load("export/ddsp_saxoo_pretrained.ts")
+sampling_rate = model.ddsp.sampling_rate.item()
+block_size = model.ddsp.block_size.item()
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -31,7 +31,7 @@ def upload_file():
         N = (sampling_rate - (len(x) % sampling_rate)) % sampling_rate
         x = np.pad(x, (0, N))
 
-        f0 = extract_pitch(x, 16000, 160) * 2
+        f0 = extract_pitch(x, 16000, 160)
         lo = extract_loudness(x, 16000, 160)
         with torch.no_grad():
             f0 = torch.from_numpy(f0).reshape(1, -1, 1).float()
